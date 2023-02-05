db00668c1a184e4076d02207a5f8d52b50e082a6
Chris O'Neil <chriso83@protonmail.com>
2023-02-05 19:13:33 +0000

    feat: provide download command
    The download command will iterate over all the videos in the database for a given channel ID and use
    `yt-dlp` to download each of them.
    
    Once the video has been downloaded, its entry in the database is updated with the path it was
    downloaded to. If the `download` command runs again, it will check if that file exists, and if so,
    it will be skipped for re-downloading. Just in case `yt-dlp` is configured with the `--no-overwrite`
    option, which has the same effect.
    
    It expects the `YT_CH_ARCHIVER_ROOT_PATH` variable to be set. Under here, it will create a directory
    with the channel name, which will then have three subdirectories:
    
    * video
    * info
    * description
    
    The reason is just to make organisation a bit tidier.
    
    For each video, we will ask `yt-dlp` to get the video along with its thumbnail, info and
    description. These get stored in the three directories above, respectively. The thumbnail gets
    stored along with the video because though `yt-dlp` is supposed to have a feature to enable the
    thumbnail to be saved to a different directory, it doesn't work.
    
    Each of these files is saved as <video-id>.<ext>. The default behaviour for `yt-dlp` is to have the
    title of the video in the file name, but this can lead to unwieldy filenames, sometimes with special
    characters. The titles for the video are stored in the info files, so we can build an index to
    navigate the saved videos, which is the next command I intend to implement.
    
    The other noteworthy thing is that `yt-dlp` is configured to use cookies from Firefox, which should
    bypass age verification.

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
 import argparse
+import glob
 import os
 import sqlite3
 import sys
 
+import yt_dlp
+
 from googleapiclient.discovery import build
+
+# According to the yt-dlp documentation, this format selection will get the
+# best mp4 video available, or failing that, the best video otherwise available.
+FORMAT_SELECTION = "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b"
 
 
 class Video:
@@ -13,6 +20,9 @@ class Video:
         self.id = id
         self.title = title
         self.saved_path = saved_path
+
+    def get_url(self):
+        return f"http://www.youtube.com/watch?v={self.id}"
 
     @staticmethod
     def from_search_response_item(youtube, item):
@@ -42,6 +52,14 @@ def get_args():
     subparsers.add_parser(
         "list-channels", help="List all the channels that have been used"
     )
+    download_parser = subparsers.add_parser(
+        "download", help="Download all videos for a channel"
+    )
+    download_parser.add_argument(
+        "channel_id",
+        type=str,
+        help="The ID of the channel whose videos you want to download",
+    )
     return parser.parse_args()
 
 
@@ -63,7 +81,7 @@ def get_channel_name(youtube, cursor, channel_id):
     return (channel_name, False)
 
 
-def create_database():
+def create_or_get_db_conn():
     if "HOME" in os.environ:
         app_data_path = os.path.join(os.environ["HOME"], ".local", "yt-ch-archiver")
     elif "APPDATA" in os.environ:
@@ -97,19 +115,41 @@ def create_database():
     return (conn, cursor)
 
 
+def get_video_list(channel_id, cursor):
+    videos = []
+    cursor.execute("SELECT * FROM videos WHERE channel_id = ?", (channel_id,))
+    rows = cursor.fetchall()
+    for row in rows:
+        video = Video.from_row(row)
+        videos.append(video)
+    return videos
+
+
+def get_full_video_path(download_path, video_id):
+    """
+    Before calling yt-dlp, it's not possible to know the file extension of the
+    file it will save. Here we will use a filter and take the first file in the
+    list (there should only be one item in the list) in the `video` subdirectory.
+
+    We have to exclude thumbnail files from the output. The documentation for
+    yt-dlp implies that thumbnails can be output to a separate path like the
+    info and description files, but it doesn't work, so the thumbnails are also
+    getting written out to the `video` subdirectory.
+    """
+    pattern = os.path.join(download_path, f"{video_id}.[!webp|jpg]*")
+    files = glob.glob(pattern)
+    return files[0]
+
+
 def process_list_command(youtube, channel_id):
-    (conn, cursor) = create_database()
+    (conn, cursor) = create_or_get_db_conn()
     (channel_name, channel_is_cached) = get_channel_name(youtube, cursor, channel_id)
 
     videos = []
     print(f"Getting video list for {channel_name}")
     if channel_is_cached:
         print(f"Retrieving {channel_name} videos from cache...")
-        cursor.execute("SELECT * FROM videos WHERE channel_id = ?", (channel_id,))
-        rows = cursor.fetchall()
-        for row in rows:
-            video = Video.from_row(row)
-            videos.append(video)
+        videos.extend(get_video_list(channel_id, cursor))
     else:
         print(f"{channel_name} is not cached. Will retrieve video list from YouTube...")
         next_page_token = None
@@ -143,10 +183,11 @@ def process_list_command(youtube, channel_id):
     conn.close()
     for video in videos:
         print(f"{video.id}: {video.title}")
+    return 0
 
 
-def process_list_channel_command(youtube):
-    (conn, cursor) = create_database()
+def process_list_channel_command():
+    (conn, cursor) = create_or_get_db_conn()
     cursor.execute("SELECT * FROM channels")
     rows = cursor.fetchall()
     for row in rows:
@@ -155,6 +196,72 @@ def process_list_channel_command(youtube):
         print(f"{id}: {name}")
     cursor.close()
     conn.close()
+    return 0
+
+
+def process_download_command(youtube, channel_id):
+    download_root_path = os.getenv("YT_CH_ARCHIVER_ROOT_PATH")
+    if not download_root_path:
+        print(
+            "The YT_CH_ARCHIVER_ROOT_PATH environment variable must be set with your API key"
+        )
+        return 1
+    (conn, cursor) = create_or_get_db_conn()
+    (channel_name, channel_is_cached) = get_channel_name(youtube, cursor, channel_id)
+    if not channel_is_cached:
+        print(
+            "Please run the `list` command first to cache a list of this channel's videos"
+        )
+        return 1
+    download_path = os.path.join(download_root_path, channel_name)
+    videos = get_video_list(channel_id, cursor)
+    cursor.close()
+    conn.close()
+
+    print(f"Attempting to download videos for {channel_name}...")
+    for video in videos:
+        if video.saved_path and os.path.exists(video.saved_path):
+            print(f"{video.id} has already been downloaded. Skipping.")
+            continue
+        ydl_opts = {
+            "continue": True,
+            "cookiesfrombrowser": ("firefox",),
+            "format": FORMAT_SELECTION,
+            "outtmpl": {
+                "default": "%(id)s.%(ext)s",
+                "description": "%(id)s.%(ext)s",
+                "infojson": "%(id)s.%(ext)s",
+            },
+            "paths": {
+                "home": os.path.join(download_path, "video"),
+                "description": os.path.join(download_path, "description"),
+                "infojson": os.path.join(download_path, "info"),
+            },
+            "nooverwrites": True,
+            "nopart": True,
+            "writedescription": True,
+            "writeinfojson": True,
+            "writethumbnail": True,
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([video.get_url()])
+            full_video_path = get_full_video_path(
+                os.path.join(download_path, "video"), video.id
+            )
+            print(
+                f"Updating {video.id} cache entry to indicate video saved at {full_video_path}"
+            )
+            (conn, cursor) = create_or_get_db_conn()
+            cursor.execute(
+                """
+                UPDATE videos SET saved_path = ? WHERE id = ?
+                """,
+                (full_video_path, video.id),
+            )
+            conn.commit()
+            cursor.close()
+            conn.close()
+    return 0
 
 
 def main():
@@ -167,9 +274,11 @@ def main():
     args = get_args()
     youtube = build("youtube", "v3", developerKey=api_key)
     if args.subcommand == "list":
-        process_list_command(youtube, args.channel_id)
+        return process_list_command(youtube, args.channel_id)
     elif args.subcommand == "list-channels":
-        process_list_channel_command(youtube)
+        return process_list_channel_command()
+    elif args.subcommand == "download":
+        return process_download_command(youtube, args.channel_id)
     return 0
 
 
